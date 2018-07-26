feb39135ca23c14f20817260c32ab126824ef439
Chribtoper <xcstolfax@gmail.com>
2018-07-26 19:40:35 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 
 
-def countdown()
+def countdown(int)
   while seconds > 0
   puts "#{seconds} SECOND(S)!"
   seconds -= 1
@@ -10,3 +10,11 @@ end
   "HAPPY NEW YEAR"
 end
 
+def countdown_with_sleep(int)
+  sleep(5)
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+end
+  "HAPPY NEW YEAR"
+end
